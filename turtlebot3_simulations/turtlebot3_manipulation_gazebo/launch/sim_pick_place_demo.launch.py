1ebdc4296a3f35aeb0ddfe2e76188386ca9a052c
KweonTJ <kweontj0701@naver.com>
2026-04-30 23:30:53 +0900

    auto: update 2026-04-30 23:30:53

--- turtlebot3_simulations/turtlebot3_manipulation_gazebo/launch/sim_pick_place_demo.launch.py
+++ turtlebot3_simulations/turtlebot3_manipulation_gazebo/launch/sim_pick_place_demo.launch.py
@@ -57,9 +57,9 @@ def generate_launch_description():
             default_value=PathJoinSubstitution([
                 FindPackageShare("turtlebot3_manipulation_gazebo"),
                 "worlds",
-                "grasp_3m_room.world",
+                "grasp_10m_room.world",
             ]),
-            description="Gazebo 3 m x 3 m room world containing the far red pick object.",
+            description="Gazebo 10 m x 10 m room world containing the far red pick object.",
         ),
         DeclareLaunchArgument(
             "gz_args",
