64fb99b4ad2074fd993154ff2aaf97631a8c25aa
KweonTJ <kweontj0701@naver.com>
2026-05-12 13:16:06 +0900

    auto: update 2026-05-12 13:16:06

--- turtlebot3_simulations/turtlebot3_manipulation_gazebo/launch/base.launch.py
+++ turtlebot3_simulations/turtlebot3_manipulation_gazebo/launch/base.launch.py
@@ -205,7 +205,7 @@ def generate_launch_description():
 
     stay_pose_msg = (
         '{joint_names: [joint1, joint2, joint3, joint4], '
-        'points: [{positions: [0.104311, 0.027612, -0.001534, -1.638291], '
+        'points: [{positions: [0.104311, 0.027612, -0.001534, -0.026078], '
         'time_from_start: {sec: 2}}]}'
     )
 
