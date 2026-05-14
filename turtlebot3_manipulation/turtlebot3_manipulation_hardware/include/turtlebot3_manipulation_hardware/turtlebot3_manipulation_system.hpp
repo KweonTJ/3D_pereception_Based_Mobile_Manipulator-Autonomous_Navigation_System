f3a74a12781eb521a88e1fd74d2da029bed8413d
KweonTJ <kweontj0701@naver.com>
2026-05-14 12:02:12 +0900

    Revert "auto: update 2026-05-14 11:50:59"
    This reverts commit ac19534bc7cd7e4d26b5b0b012486fe15a5474c3.

--- turtlebot3_manipulation/turtlebot3_manipulation_hardware/include/turtlebot3_manipulation_hardware/turtlebot3_manipulation_system.hpp
+++ turtlebot3_manipulation/turtlebot3_manipulation_hardware/include/turtlebot3_manipulation_hardware/turtlebot3_manipulation_system.hpp
@@ -82,8 +82,6 @@ private:
 
   int32_t gripper_acceleration_;
   int32_t gripper_velocity_;
-  double wheel_left_direction_{1.0};
-  double wheel_right_direction_{1.0};
 
   std::unique_ptr<OpenCR> opencr_;
 
