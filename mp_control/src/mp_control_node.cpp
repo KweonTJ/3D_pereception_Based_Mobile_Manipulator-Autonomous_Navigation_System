729fa37b182f855809e8aef23343cf36f7def322
KweonTJ <kweontj0701@naver.com>
2026-05-17 19:46:43 +0900

    auto: update 2026-05-17 19:46:43

--- mp_control/src/mp_control_node.cpp
+++ mp_control/src/mp_control_node.cpp
@@ -609,6 +609,7 @@ private:
     triangulation_joint_extend_sent_ = false;
     servo_started_after_pregrasp_ = false;
     triangulation_joint_extend_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
+    triangulation_joint_extend_wait_s = 0.0;
     publishEefAutoInitEnable(false);
     publishBaseHold(false);
     last_eef_init_bbox_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
