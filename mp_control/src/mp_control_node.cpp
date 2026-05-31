8430643ad4a3ee4fb37bd255a76d573bd0fcc0c4
KweonTJ <kweontj0701@naver.com>
2026-05-31 23:51:23 +0900

    auto: update 2026-05-31 23:51:23

--- mp_control/src/mp_control_node.cpp
+++ mp_control/src/mp_control_node.cpp
@@ -624,6 +624,7 @@ private:
     joint_pregrasp_sent_ = false;
     joint_pregrasp_done_ = false;
     joint_pregrasp_target_.reset();
+    joint_pregrasp_controller_target_.reset();
     joint_pregrasp_start_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     joint_pregrasp_last_publish_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     resetEefRefinementMotionState();
@@ -661,6 +662,7 @@ private:
     joint_pregrasp_sent_ = false;
     joint_pregrasp_done_ = false;
     joint_pregrasp_target_.reset();
+    joint_pregrasp_controller_target_.reset();
     joint_pregrasp_start_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     joint_pregrasp_last_publish_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     resetEefRefinementMotionState();
