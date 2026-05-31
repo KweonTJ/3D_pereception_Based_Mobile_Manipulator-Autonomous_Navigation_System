46fb279f1854d13e542f33b6dd2807d9235cd6fb
KweonTJ <kweontj0701@naver.com>
2026-06-01 02:27:51 +0900

    auto: update 2026-06-01 02:27:51

--- depth_perception/astra_mini_calibration/scripts/camera_info_from_json.py
+++ depth_perception/astra_mini_calibration/scripts/camera_info_from_json.py
@@ -114,9 +114,12 @@ def main() -> None:
     node = CameraInfoFromJsonNode()
     try:
         rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        if rclpy.ok():
+            rclpy.shutdown()
 
 
 if __name__ == '__main__':
