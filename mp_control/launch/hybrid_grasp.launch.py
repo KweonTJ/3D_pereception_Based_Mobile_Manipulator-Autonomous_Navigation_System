b3e96de97601581c19d9b2597f52d4153a241e69
KweonTJ <kweontj0701@naver.com>
2026-05-31 21:53:01 +0900

    auto: update 2026-05-31 21:53:01

--- mp_control/launch/hybrid_grasp.launch.py
+++ mp_control/launch/hybrid_grasp.launch.py
@@ -50,6 +50,10 @@ def generate_launch_description():
             "output_topic": joint_trajectory_output_topic,
             "joint_state_topic": "/joint_states",
             "reverse_joint_names": ["joint3"],
+            "preserve_roll_sum": True,
+            "roll_parent_joint": "joint2",
+            "roll_reversed_joint": "joint3",
+            "roll_compensation_joint": "joint4",
         }],
         condition=IfCondition(start_joint_trajectory_transformer),
     )
