e367e3faa79955a0601efd6d7724fe14544f09c5
KweonTJ <kweontj0701@naver.com>
2026-06-08 17:22:00 +0900

    auto: update 2026-06-08 17:22:00

--- mp_control/launch/real_pick_place.launch.py
+++ mp_control/launch/real_pick_place.launch.py
@@ -269,7 +269,7 @@ def generate_launch_description():
             "bbox_padding_scale_y": 1.15,
             "continuous_publish": True,
             "continuous_publish_period_s": 0.35,
-            "reuse_last_bbox_on_loss": True,
+            "reuse_last_bbox_on_loss": False,
             "reuse_last_bbox_max_age_s": 1.0,
             "lock_first_bbox": False,
         }],
