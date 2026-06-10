2749a731cc49f09a655a46c402d8bfbd0cdfe2d5
KweonTJ <kweontj0701@naver.com>
2026-06-10 10:31:45 +0900

    auto: update 2026-06-10 10:31:45

--- mp_control/src/mp_control_node.cpp
+++ mp_control/src/mp_control_node.cpp
@@ -4788,7 +4788,7 @@ private:
   double gripperPositionForGap(double gap_m) const
   {
     const double joint_position =
-      0.5 * std::max(0.0, gap_m) - gripper_finger_home_half_gap_m_;
+      0.05 * std::max(0.0, gap_m) - gripper_finger_home_half_gap_m_;
     return clampValue(joint_position, gripper_min_position_, gripper_max_position_);
   }
 
