45afdce6ae7bafea94700a7d4db05189565fcf55
KweonTJ <kweontj0701@naver.com>
2026-06-10 18:17:33 +0900

    auto: update 2026-06-10 18:17:33

--- depth_perception/aruco_eef_tracker/setup.py
+++ depth_perception/aruco_eef_tracker/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'aruco_eef_tracker'
 
@@ -7,16 +9,17 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ktj',
     maintainer_email='kweontj0701@naver.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='EEF camera ArUco marker pose tracker for manipulator grasping.',
+    license='Apache License 2.0',
     extras_require={
         'test': [
             'pytest',
@@ -24,6 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            "aruco_eef_tracker_node = aruco_eef_tracker.aruco_eef_tracker_node:main",
         ],
     },
 )
