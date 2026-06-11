85c44af534f5f154c579698d030170dba2d084c0
KweonTJ <kweontj0701@naver.com>
2026-06-11 23:06:09 +0900

    auto: update 2026-06-11 23:06:09

--- mp_control/src/mp_control_node.cpp
+++ mp_control/src/mp_control_node.cpp
@@ -857,6 +857,7 @@ private:
     eef_forward_last_joint_nudge_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     handoff_stage_start_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
     handoff_last_publish_stamp_ = rclcpp::Time(0, 0, RCL_ROS_TIME);
+    handoff_lift_duration_s_ = std::max(0.1, handoff_lift_duration_s_);
     handoff_lift_controller_target_.reset();
     handoff_place_controller_target_.reset();
     handoff_stay_controller_target_.reset();
