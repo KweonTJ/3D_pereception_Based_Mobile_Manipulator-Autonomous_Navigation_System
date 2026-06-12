e75873a9cab45c0c1e8b300a7b947070ea73bd1f
KweonTJ <kweontj0701@naver.com>
2026-06-12 22:33:03 +0900

    auto: update 2026-06-12 22:33:03

--- depth_perception/aruco_mp_bridge/aruco_mp_bridge/aruco_to_mp_control_bridge.py
+++ depth_perception/aruco_mp_bridge/aruco_mp_bridge/aruco_to_mp_control_bridge.py
@@ -45,6 +45,8 @@ class ArucoToMpControlBridge(Node):
             self.declare_parameter("continuous_start_publish", True).value)
         self.publish_close_range_ready = bool(
             self.declare_parameter("publish_close_range_ready", True).value)
+        self.locked_object_xyz = None
+        self.start_sent_once = False
 
         latched_qos = QoSProfile(
             history=HistoryPolicy.KEEP_LAST,
